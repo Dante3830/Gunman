9d311ce7241e678cb90093b4318f806e51f82ba8
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2022-12-30 13:59:47 -0300

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+#include <iostream>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+using namespace sf;

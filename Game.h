70be1aabfebe86d7d01f23283dc0b80f5d0eeb97
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2023-02-14 19:06:34 -0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,217 +1,133 @@
 #pragma once
-
 #include "stdafx.h"
-#include "Bat.h"
-#include "Ball.h"
-#include "AIBat.h"
-#include "Constants.h"
+#include "Enemy.h"
+#include "Ally.h"
 
-class Game {
+class Position {
 private:
-    Font font;
-    SoundBuffer buffer;
-    Sound music;
+	Enemy* _enemy = NULL;
+	Ally* _ally = NULL;
 
-    Text title;
-    Text startMessage;
+	float coordX;
+	float coordY;
 
-    Text _BatScore;
-    Text _aiBatScore;
-
-    Text finishMessage1;
-    Text finishMessage2;
-
-    void updateBatScore() {
-        char bts[10];
-        _itoa_s(batScore, bts, 10);
-        _BatScore.setString(bts);
-    }
-
-    void updateAiBatScore() {
-        char aibts[10];
-        _itoa_s(aiBatScore, aibts, 10);
-        _aiBatScore.setString(aibts);
-    }
+	bool eneShot = false;
 
 public:
 
-    Game() {
-        // Mensaje del inicio
-        title.setCharacterSize(90);
-        title.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
-        title.setFont(font);
-        title.setFillColor(Color::Color(21, 182, 185));
-        title.setString("     PONG");
+	Position() {
+		
+	}
 
-        startMessage.setCharacterSize(30);
-        startMessage.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
-        startMessage.setFont(font);
-        startMessage.setString("\n\n\n\n\n\n\n\n      Presiona las flechas para iniciar");
+	bool onTop(float x, float y) {
+		if (_enemy) {
+			if (_enemy->onTop(x, y)) {
+				_enemy->Defeat();
+				return true;
+			}
+		}
+		else if (_ally) {
+			if (_ally->onTop(x, y)) {
+				_ally->Hurt();
+				return true;
+			}
+		}
 
-        // Mensaje de victoria
-        finishMessage1.setCharacterSize(30);
-        finishMessage1.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
-        finishMessage1.setFont(font);
-        finishMessage1.setFillColor(Color::White);
-        finishMessage1.setString("                 ¡Ganaste!\n\n   Presiona cualquier tecla para reiniciar");
+		return false;
+	}
 
-        // Mensaje de derrota
-        finishMessage2.setCharacterSize(30);
-        finishMessage2.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
-        finishMessage2.setFont(font);
-        finishMessage2.setFillColor(Color::White);
-        finishMessage2.setString("                 Perdiste...\n\n   Presiona cualquier tecla para reiniciar");
+	bool hasEnemy() {
+		if (_enemy) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	bool hasAlly() {
+		if (_ally) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
-        //Mostrar puntaje del jugador
-        _BatScore.setCharacterSize(60);
-        _BatScore.setPosition(800.0f, 100.0f);
-        _BatScore.setFont(font);
-        _BatScore.setFillColor(Color::White);
+	void defeatEnemy() {
+		delete _enemy;
+		_enemy = NULL;
+	}
 
-        //Mostrar puntaje de la IA
-        _aiBatScore.setCharacterSize(60);
-        _aiBatScore.setPosition(200.0f, 100.0f);
-        _aiBatScore.setFont(font);
-        _aiBatScore.setFillColor(Color::White);
+	void hurtAlly() {
+		delete _ally;
+		_ally = NULL;
+	}
 
-        updateBatScore();
-        updateAiBatScore();
-    }
+	void Draw(RenderWindow* _wnd) {
+		if (_enemy) {
+			_enemy->Draw(_wnd);
+		}
 
-    void musicAndFont() {
-        //Cargando la fuente 
-        font.loadFromFile("VCR_OSD_MONO.ttf");
+		if (_ally) {
+			_ally->Draw(_wnd);
+		}
+	}
 
-        //Seteando la musica
-        buffer.loadFromFile("MainMusic.wav");
-        music.setBuffer(buffer);
-    }
+	void setCoordenates(int position) {
+		switch (position) {
+		case 0:
+			//Ventana superior izquierda
+			coordX = 150.0f;
+			coordY = 150.0f;
+			break;
+		case 1:
+			//Ventana superior derecha
+			coordX = 550.0f;
+			coordY = 150.0f;
+			break;
+		case 2:
+			//Ventana inferior izquierda
+			coordX = 150.0f;
+			coordY = 370.0f;
+			break;
+		case 3:
+			//Puerta de entrada
+			coordX = 330.0f;
+			coordY = 370.0f;
+			break;
+		case 4:
+			//Ventana inferior derecha
+			coordX = 550.0f;
+			coordY = 370.0f;
+			break;
+		}
+	}
 
-    //void logic() {
+	bool enemyFire(){
+		return eneShot;
+	}
 
-    //}
+	void update(RenderWindow* _wnd) {
+		if (_enemy) {
+			bool enemyShoot = _enemy->Update(_wnd);
 
-    void pong() {
-        RenderWindow window(VideoMode(windowWidth, windowHeight), "Pong");
+			eneShot = enemyShoot;
 
-        //Creando los objetos
-        Bat bat(20, windowHeight / 2);
-        Ball ball(windowWidth / 2 + 50, windowHeight / 2);
-        AIBat aibat(windowWidth - 40, windowHeight / 2);
+		} else if (_ally) {
+			bool allyAlive = _ally->Update(_wnd);
 
-        musicAndFont();
+			if (!allyAlive) {
+				hurtAlly();
+			}
 
-        while (window.isOpen()) {
+		} else {
+			if (rand() % 100 < 50) {
+				_enemy = new Enemy(coordX, coordY);
+			} else if (rand() % 100 < 75) {
+				_ally = new Ally(coordX, coordY);
+			}
+		}
+	}
 
-            // Limpiar y dibujar fondo
-            window.clear(Color(34, 34, 34, 100));
-
-            window.setFramerateLimit(480);
-
-            music.play();
-
-            Event event;
-            while (window.pollEvent(event)) {
-                if (event.type == event.Closed) {
-                    window.close();
-                }
-            }
-
-            // Controles del usuario
-            if (((event.type == event.KeyPressed) && event.key.code == Keyboard::Up)) {
-                bat.moveBatUp();
-            }
-            else if (((event.type == event.KeyPressed) && event.key.code == Keyboard::Down)) {
-                bat.moveBatDown();
-            }
-
-             // Logica
-            ball.reboundSides();
-            ball.passLeft();
-            ball.passRight();
-
-            if (ball.getBallFloatRect().intersects(bat.getBatFloatRect())) {
-                ball.reboundBatorAI();
-            }
-
-            if (ball.getBallFloatRect().intersects(aibat.getAIBatFloatRect())) {
-                ball.reboundBatorAI();
-            }
-
-            if (ball.getBallFloatRect().top > (aibat.getAIBatFloatRect().top) + 50) {
-                if (aiBatCounter % 60 == 0) {
-                    aibat.moveAIBatDown();
-                }
-            }
-
-            if (ball.getBallFloatRect().top < (aibat.getAIBatFloatRect().top) + 50) {
-                if (aiBatCounter % 60 == 0) {
-                    aibat.moveAIBatUp();
-                }
-            }
-
-            if (ball.getBallPosition.x > windowWidth) {
-                aibat.AIBatSpeedReverse();
-            }
-
-             // Actualizacion
-            if (aiBatCounter == 1000000) {
-                aiBatCounter = 0;
-            }
-
-            aiBatCounter++;
-            
-            ball.update();
-            bat.update();
-            aibat.update();
-
-            // Textos e iniciacion //
-
-            if (batScore == 10) {
-                //En caso de victoria
-                window.draw(finishMessage1);
-                ball.stop();
-
-                if (event.type == event.KeyPressed) {
-                    aiBatScore = 0;
-                    batScore = 0;
-                    ball.resetVelocity();
-                }
-
-            } else if (aiBatScore == 10) {
-                //En caso de derrota
-                window.draw(finishMessage2);
-                ball.stop();
-
-                if (event.type == event.KeyPressed) {
-                    aiBatScore = 0;
-                    batScore = 0;
-                    ball.resetVelocity();
-                }
-
-            } else if (batScore == -1) {
-                window.draw(title);
-                window.draw(startMessage);
-                ball.stop();
-
-                if (event.type == event.KeyPressed) {
-                    aiBatScore = 0;
-                    batScore = 0;
-
-                    //Mostrar puntaje de cada uno
-                    window.draw(_BatScore);
-                    window.draw(_aiBatScore);
-
-                    ball.resetVelocity();
-                }
-
-            } else {
-                window.draw(bat.getBatObject());
-                window.draw(ball.getBallObject());
-                window.draw((aibat.getAIBatObject()));
-            }
-
-            window.display();
-        }
-    }
 };
